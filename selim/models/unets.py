25261999fdef019afad712f2708715c8f15173c9
Vole1 <Vole1@users.noreply.github.com>
2020-05-24 13:58:25 +0300

    Fixing dropouts in xception_mc #4

--- selim/models/unets.py
+++ selim/models/unets.py
@@ -246,40 +246,40 @@ def xception_fpn(input_shape, channels=1, weights='imagenet', activation="sigmoi
     return model
 
 
-# def xception_fpn_mc(input_shape, channels=1, p=0.3, weights='imagenet', activation="sigmoid"):
-#     xception_mc = Xception_mc_dropout(input_shape=input_shape, p=p, weights=weights, include_top=False)
-#     conv1 = xception_mc.get_layer("block1_conv2_act").output
-#     conv2 = xception_mc.get_layer("block3_sepconv2_bn").output
-#     conv3 = xception_mc.get_layer("block4_sepconv2_bn").output
-#     conv3 = Activation("relu")(conv3)
-#     conv4 = xception_mc.get_layer("block13_sepconv2_bn").output
-#     conv4 = Activation("relu")(conv4)
-#     conv5 = xception_mc.get_layer("block14_sepconv2_act").output
-#
-#     P1, P2, P3, P4, P5 = create_pyramid_features(conv1, conv2, conv3, conv4, conv5)
-#     x = concatenate(
-#         [
-#             prediction_fpn_block(P5, "P5", (8, 8)),
-#             prediction_fpn_block(P4, "P4", (4, 4)),
-#             prediction_fpn_block(P3, "P3", (2, 2)),
-#             prediction_fpn_block(P2, "P2"),
-#         ]
-#     )
-#     x = conv_bn_relu(x, 256, 3, (1, 1), name="aggregation")
-#     x = decoder_block_no_bn(x, 128, conv1, 'up4')
-#     x = UpSampling2D()(x)
-#     x = conv_relu(x, 64, 3, (1, 1), name="up5_conv1")
-#     x = conv_relu(x, 64, 3, (1, 1), name="up5_conv2")
-#     if activation == 'softmax':
-#         name = 'mask_softmax'
-#         x = Conv2D(channels, (1, 1), activation=activation, name=name)(x)
-#     else:
-#         x = Conv2D(channels, (1, 1), activation=activation, name="mask")(x)
-#     model = Model(xception_mc.input, x)
-#     return model
-
-
 def xception_fpn_mc(input_shape, channels=1, p=0.3, weights='imagenet', activation="sigmoid"):
+    xception_mc = Xception_mc_dropout(input_shape=input_shape, p=p, weights=weights, include_top=False)
+    conv1 = xception_mc.get_layer("block1_conv2_act").output
+    conv2 = xception_mc.get_layer("block3_sepconv2_bn").output
+    conv3 = xception_mc.get_layer("block4_sepconv2_bn").output
+    conv3 = Activation("relu")(conv3)
+    conv4 = xception_mc.get_layer("block13_sepconv2_bn").output
+    conv4 = Activation("relu")(conv4)
+    conv5 = xception_mc.get_layer("block14_sepconv2_act").output
+
+    P1, P2, P3, P4, P5 = create_pyramid_features(conv1, conv2, conv3, conv4, conv5)
+    x = concatenate(
+        [
+            prediction_fpn_block(P5, "P5", (8, 8)),
+            prediction_fpn_block(P4, "P4", (4, 4)),
+            prediction_fpn_block(P3, "P3", (2, 2)),
+            prediction_fpn_block(P2, "P2"),
+        ]
+    )
+    x = conv_bn_relu(x, 256, 3, (1, 1), name="aggregation")
+    x = decoder_block_no_bn(x, 128, conv1, 'up4')
+    x = UpSampling2D()(x)
+    x = conv_relu(x, 64, 3, (1, 1), name="up5_conv1")
+    x = conv_relu(x, 64, 3, (1, 1), name="up5_conv2")
+    if activation == 'softmax':
+        name = 'mask_softmax'
+        x = Conv2D(channels, (1, 1), activation=activation, name=name)(x)
+    else:
+        x = Conv2D(channels, (1, 1), activation=activation, name="mask")(x)
+    model = Model(xception_mc.input, x)
+    return model
+
+
+def xception_fpn_mc1(input_shape, channels=1, p=0.3, weights='imagenet', activation="sigmoid"):
     xception_mc = Xception_mc_dropout(input_shape=input_shape, p=p, weights=weights, include_top=False)
     conv1 = xception_mc.get_layer("block1_conv2_act").output
     conv2 = xception_mc.get_layer("block3_sepconv2_bn").output
