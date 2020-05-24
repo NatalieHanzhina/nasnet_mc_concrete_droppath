cee06060f958e917b37c3b1e3f123412f47b63a0
Vole1 <Vole1@users.noreply.github.com>
2020-05-24 13:12:19 +0300

    Fixing dropouts in xception_mc #2

--- selim/models/xception_padding_mc_dropout.py
+++ selim/models/xception_padding_mc_dropout.py
@@ -150,12 +150,10 @@ def Xception_mc_dropout(include_top=True, p=0.3, weights='imagenet',
     x = BatchNormalization(name='block1_conv1_bn')(x)
     x = Dropout(p)(x, training=True)
     x = Activation('relu', name='block1_conv1_act')(x)
-    x = Dropout(p)(x, training=True)
     x = Conv2D(64, (3, 3), use_bias=False, name='block1_conv2', padding='same')(x)
     x = BatchNormalization(name='block1_conv2_bn')(x)
     x = Dropout(p)(x, training=True)
     x = Activation('relu', name='block1_conv2_act')(x)
-    x = Dropout(p)(x, training=True)
 
     residual = Conv2D(128, (1, 1), strides=(2, 2),
                       padding='same', use_bias=False)(x)
@@ -167,15 +165,12 @@ def Xception_mc_dropout(include_top=True, p=0.3, weights='imagenet',
     x = BatchNormalization(name='block2_sepconv1_bn')(x)
     x = Dropout(p)(x, training=True)
     x = Activation('relu', name='block2_sepconv2_act')(x)
-    x = Dropout(p)(x, training=True)
     x = SeparableConv2D(128, (3, 3), padding='same', use_bias=False, name='block2_sepconv2')(x)
     x = BatchNormalization(name='block2_sepconv2_bn')(x)
     x = Dropout(p)(x, training=True)
 
     x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block2_pool')(x)
-    x = Dropout(p)(x, training=True)
     x = layers.add([x, residual])
-    x = Dropout(p)(x, training=True)
 
     residual = Conv2D(256, (1, 1), strides=(2, 2),
                       padding='same', use_bias=False)(x)
@@ -189,15 +184,12 @@ def Xception_mc_dropout(include_top=True, p=0.3, weights='imagenet',
     x = BatchNormalization(name='block3_sepconv1_bn')(x)
     x = Dropout(p)(x, training=True)
     x = Activation('relu', name='block3_sepconv2_act')(x)
-    x = Dropout(p)(x, training=True)
     x = SeparableConv2D(256, (3, 3), padding='same', use_bias=False, name='block3_sepconv2')(x)
     x = BatchNormalization(name='block3_sepconv2_bn')(x)
     x = Dropout(p)(x, training=True)
 
     x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block3_pool')(x)
-    x = Dropout(p)(x, training=True)
     x = layers.add([x, residual])
-    x = Dropout(p)(x, training=True)
 
     residual = Conv2D(728, (1, 1), strides=(2, 2),
                       padding='same', use_bias=False)(x)
@@ -205,37 +197,30 @@ def Xception_mc_dropout(include_top=True, p=0.3, weights='imagenet',
     residual = Dropout(p)(residual, training=True)
 
     x = Activation('relu', name='block4_sepconv1_act')(x)
-    x = Dropout(p)(x, training=True)
     x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name='block4_sepconv1')(x)
     x = BatchNormalization(name='block4_sepconv1_bn')(x)
     x = Dropout(p)(x, training=True)
     x = Activation('relu', name='block4_sepconv2_act')(x)
-    x = Dropout(p)(x, training=True)
     x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name='block4_sepconv2')(x)
     x = BatchNormalization(name='block4_sepconv2_bn')(x)
     x = Dropout(p)(x, training=True)
 
     x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block4_pool')(x)
-    x = Dropout(p)(x, training=True)
     x = layers.add([x, residual])
-    x = Dropout(p)(x, training=True)
 
     for i in range(8):
         residual = x
         prefix = 'block' + str(i + 5)
 
         x = Activation('relu', name=prefix + '_sepconv1_act')(x)
-        x = Dropout(p)(x, training=True)
         x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv1')(x)
         x = BatchNormalization(name=prefix + '_sepconv1_bn')(x)
         x = Dropout(p)(x, training=True)
         x = Activation('relu', name=prefix + '_sepconv2_act')(x)
-        x = Dropout(p)(x, training=True)
         x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv2')(x)
         x = BatchNormalization(name=prefix + '_sepconv2_bn')(x)
         x = Dropout(p)(x, training=True)
         x = Activation('relu', name=prefix + '_sepconv3_act')(x)
-        x = Dropout(p)(x, training=True)
         x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name=prefix + '_sepconv3')(x)
         x = BatchNormalization(name=prefix + '_sepconv3_bn')(x)
         x = Dropout(p)(x, training=True)
@@ -248,32 +233,26 @@ def Xception_mc_dropout(include_top=True, p=0.3, weights='imagenet',
     residual = Dropout(p)(residual, training=True)
 
     x = Activation('relu', name='block13_sepconv1_act')(x)
-    x = Dropout(p)(x, training=True)
     x = SeparableConv2D(728, (3, 3), padding='same', use_bias=False, name='block13_sepconv1')(x)
     x = BatchNormalization(name='block13_sepconv1_bn')(x)
     x = Dropout(p)(x, training=True)
     x = Activation('relu', name='block13_sepconv2_act')(x)
-    x = Dropout(p)(x, training=True)
     x = SeparableConv2D(1024, (3, 3), padding='same', use_bias=False, name='block13_sepconv2')(x)
     x = BatchNormalization(name='block13_sepconv2_bn')(x)
     x = Dropout(p)(x, training=True)
 
     x = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='block13_pool')(x)
-    x = Dropout(p)(x, training=True)
     x = layers.add([x, residual])
-    x = Dropout(p)(x, training=True)
 
     x = SeparableConv2D(1536, (3, 3), padding='same', use_bias=False, name='block14_sepconv1')(x)
     x = BatchNormalization(name='block14_sepconv1_bn')(x)
     x = Dropout(p)(x, training=True)
     x = Activation('relu', name='block14_sepconv1_act')(x)
-    x = Dropout(p)(x, training=True)
 
     x = SeparableConv2D(2048, (3, 3), padding='same', use_bias=False, name='block14_sepconv2')(x)
     x = BatchNormalization(name='block14_sepconv2_bn')(x)
     x = Dropout(p)(x, training=True)
     x = Activation('relu', name='block14_sepconv2_act')(x)
-    x = Dropout(p)(x, training=True)
 
     if include_top:
         x = GlobalAveragePooling2D(name='avg_pool')(x)
