5ecd7504931ed730cd509fc374998a030e448a93
Vole1 <studying48@gmail.com>
2020-06-01 08:04:32 +0200

    updating repo to implement test with uncertainty

---
+++ selim/test_xception_mc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+########## Xception FPN #############
+python test.py \
+--num_workers 8 \
+--channels 3 \
+--pretrained_weights None \
+--preprocessing_function caffe \
+--network xception_fpn_mc \
+--batch_size 1 \
+--out_channels 2 \
+--models best_mc_xception_fpn_mc.h5
