98356371538dbf456cbf0bd35a472c5f3c270b4a
Vole1 <studying48@gmail.com>
2020-06-10 16:22:55 +0200

    Training and testing files addition

---
+++ selim/test_finetuned_xception_nii_brtsv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+########## Xception FPN #############
+python test.py \
+--num_workers 8 \
+--channels 4 \
+--pretrained_weights None \
+--preprocessing_function caffe \
+--network xception_fpn \
+--batch_size 3 \
+--out_channels 2 \
+--test_images_dir /media/disk1/mkashirin/Brtsv_exp_test/images \
+--test_masks_dir /media/disk1/mkashirin/Brtsv_exp_test/masks \
+--models 2nd_epoch_nii_nrh_ft_xception_fpn.h5
