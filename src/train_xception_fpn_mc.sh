380b2bccf83d02a3eb65b4f8c99585d994e44d49
Vole1 <studying48@gmail.com>
2021-06-22 03:47:16 +0000

    changes to training config files

--- src/train_xception_fpn_mc.sh
+++ src/train_xception_fpn_mc.sh
@@ -3,15 +3,21 @@
 ##################### Xception FPN with Sigmoid activation ##############################
 
 python train.py \
+--channels 4 \
+--pretrained_weights imagenet \
 --num_workers 8  \
---network xception_fpn_mc \
+--network xception_fpn_mc  \
+--alias nii_mc_do=0.3_  \
 --freeze_till_layer input_1  \
 --loss double_head_loss  \
 --optimizer adam  \
 --learning_rate 0.0001  \
+--dropout_rate 0.3  \
 --decay 0.0001  \
---batch_size 12  \
+--batch_size 14  \
 --steps_per_epoch 500 \
 --epochs 300 \
 --preprocessing_function caffe \
---log_dir xception_fpn_mc
+--images_dir /mnt/tank/scratch/mkashirin/datasets/mri_data/data_nii/images \
+--masks_dir /mnt/tank/scratch/mkashirin/datasets/mri_data/data_nii/masks \
+--log_dir xception_fpn_mc_do=0.3_nii
--- src/train_xception_fpn_nii.sh
+++ src/train_xception_fpn_nii.sh
@@ -13,10 +13,10 @@ python train.py \
 --optimizer adam  \
 --learning_rate 0.0001  \
 --decay 0.0001  \
---batch_size 16 \
+--batch_size 14 \
 --steps_per_epoch 500 \
 --epochs 100 \
 --preprocessing_function caffe \
---images_dir /media/disk1/mkashirin/data_nii/images \
---masks_dir /media/disk1/mkashirin/data_nii/masks \
+--images_dir /mnt/tank/scratch/mkashirin/datasets/mri_data/data_nii/images \
+--masks_dir /mnt/tank/scratch/mkashirin/datasets/mri_data/data_nii/masks \
 --log_dir xception_fpn_nii
